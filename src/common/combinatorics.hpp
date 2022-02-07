4edf7a408a63ca10f7087680b4216b13a9268030
Ivaschenko Dmitry <ivaschenkoda144@gmail.com>
2022-02-07 14:02:23 +0600

    add isPositive function

--- src/common/combinatorics.hpp
+++ src/common/combinatorics.hpp
@@ -8,26 +8,52 @@ namespace pt
 {
 	namespace common 
 	{
-		unsigned int factorial(unsigned int& num_el) 
+		template<class NumericType>
+		auto constexpr factorial(NumericType& num_el) -> std::uint64_t
 		{
-			unsigned int result = 1;
 
-			if (num_el == 0) return 1;
-
-			for (unsigned int i = 0; i <= num_el; ++i)
+			if (true == isPositiveTypename(num_el) && num_el != 0)
 			{
-				result = result * i;
+				std::uint64_t result = 1;
+
+				for (NumericType i = 0; i <= num_el; ++i)
+				{
+					result = result * i;
+				}
+				return result;
 			}
-			return result;
+			return 1;
 		}
 
-		template<typename T>
-		bool checkVariable(T variable) 
+		template<class NumericType>
+		auto constexpr isPositiveTypename(NumericType& type_name) -> bool
 		{
-			if (typeid(T).name() == typeid(float).name()) 
-				return true;
 
-			if (typeid(T).name() == typeid(double).name()) 
+			if (std::is_same<type_name, unsigned int>)
+				return true;
+			if (std::is_same<type_name, uint8_t>)
+				return true;
+			if (std::is_same<type_name, uint16_t>)
+				return true;
+			if (std::is_same<type_name, uint32_t>)
+				return true;
+			if (std::is_same<type_name, uint64_t>)
+				return true;
+			if (std::is_same<type_name, uint_fast8_t>)
+				return true;
+			if (std::is_same<type_name, uint_fast16_t>)
+				return true;
+			if (std::is_same<type_name, uint_fast32_t>)
+				return true;
+			if (std::is_same<type_name, uint_fast64_t>)
+				return true;
+			if (std::is_same<type_name, uint_least8_t>)
+				return true;
+			if (std::is_same<type_name, uint_least16_t>)
+				return true;
+			if (std::is_same<type_name, uint_least32_t>)
+				return true;
+			if (std::is_same<type_name, uint_least64_t>)
 				return true;
 
 			return false;
