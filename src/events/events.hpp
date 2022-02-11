74dc3647af1e635f35f864275ec085b678f4c96a
Ivaschenko Dmitry <ivaschenkoda144@gmail.com>
2022-02-11 12:34:18 +0600

    add cmake build

--- src/events/events.hpp
+++ src/events/events.hpp
@@ -1,39 +1,5 @@
 #ifndef EVENTS_HPP
 #define EVENTS_HPP
 
-#include <vector>
-
-typedef float chance;
-
-class Event 
-{
-private:
-	chance m_Probability;
-
-public:
-
-	chance chance()
-	{
-		return m_Probability;
-	}
-};
-
-class EventsGroup
-{
-private:
-	std::vector<Event> m_Pool;
-
-
-public:
-
-
-};
-
-class EventsFullGroup;
-
-class EventStack
-{
-	std::vector<EventsGroup>
-};
 
 #endif // !EVENTS_HPP
