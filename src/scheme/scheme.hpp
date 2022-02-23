7f7f77f76546e3da73faf492a4719e5b9173d029
Ivaschenko Dmitry <ivaschenkoda144@gmail.com>
2022-02-23 13:49:05 +0600

    add scheme

---
+++ src/scheme/scheme.hpp
@@ -0,0 +1,63 @@
+#ifndef SCHEME_HPP
+#define SCHEME_HPP
+
+struct Scheme
+{
+	std::vector<std::vector<int>> _sc;
+	std::vector<int> _sc_filter;
+	unsigned long long _scsize{}; // it is for filter what is 
+	int _sc_max{};
+
+	Scheme() {};
+
+	void init(int data_size)
+	{
+		std::vector<int> temp;
+
+		sizeScheme(data_size);
+
+		_sc_max = data_size;
+		_sc.reserve(_scsize);
+		_sc_filter.reserve(_scsize);
+
+		backtrackIndices(0, data_size, temp);
+
+		sortScheme();
+	}
+
+	void sizeScheme(int& sz)
+	{
+		_scsize = 1;
+		for (int i = 1; i != sz + 1; ++i)
+			_scsize *= i;
+	}
+
+	void backtrackIndices(int idx, int& data_size, std::vector<int>& temp)
+	{
+
+		std::sort(temp.begin(), temp.end()); // need refactoring
+
+		if (idx == data_size)
+		{
+			_sc.push_back(temp);
+		}
+		else
+		{
+			temp.push_back(idx);
+			backtrackIndices(idx + 1, data_size, temp);
+
+			temp.pop_back();
+			backtrackIndices(idx + 1, data_size, temp);
+		}
+	}
+
+	void sortScheme()
+	{
+		std::sort(_sc.begin(), _sc.end(),
+			[](std::vector<int>& a, std::vector<int>& b) {
+				return a.size() < b.size();
+			});
+	}
+};
+
+#endif // !SCHEME_HPP
